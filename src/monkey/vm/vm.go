bc7690e0fb4d29933f5a979168e4050d9da717ec
Khongchai <world1955@hotmail.com>
2024-12-07 23:05:35 +0700

    can now run first class functions

--- src/monkey/vm/vm.go
+++ src/monkey/vm/vm.go
@@ -92,6 +92,14 @@ func (vm *VM) Run() error {
 			}
 			frame := NewFrame(fn)
 			vm.pushFrame(frame)
+		case code.OpReturn:
+			vm.popFrame()
+			vm.pop()
+
+			err := vm.push(Null)
+			if err != nil {
+				return err
+			}
 		case code.OpReturnValue:
 			returnValue := vm.pop()
 
