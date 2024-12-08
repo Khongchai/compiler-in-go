e07ac29b41f50ad8256908cae4b00993d9cb7717
Khongchai <world1955@hotmail.com>
2024-12-08 16:29:58 +0700

    about to begin chapter 9 on closures

--- src/monkey/vm/vm.go
+++ src/monkey/vm/vm.go
@@ -89,7 +89,7 @@ func (vm *VM) Run() error {
 			numArgs := code.ReadUint8(ins[ip+1:])
 			vm.currentFrame().ip += 1 // op call operand
 
-			err := vm.callFunction(int(numArgs))
+			err := vm.executeCall(int(numArgs))
 			if err != nil {
 				return err
 			}
@@ -180,6 +180,16 @@ func (vm *VM) Run() error {
 			}
 		case code.OpPop:
 			vm.pop()
+		case code.OpGetBuiltin:
+			builtinIndex := code.ReadUint8(ins[ip+1:])
+			vm.currentFrame().ip += 1
+
+			definition := object.Builtins[builtinIndex]
+
+			err := vm.push(definition.Builtin)
+			if err != nil {
+				return err
+			}
 		case code.OpGetGlobal:
 			globalIndex := code.ReadUint16(ins[ip+1:])
 			vm.currentFrame().ip += 2
@@ -243,12 +253,20 @@ func (vm *VM) Run() error {
 	return nil
 }
 
-func (vm *VM) callFunction(numArgs int) error {
-	fn, ok := vm.stack[vm.sp-1-int(numArgs)].(*object.CompiledFunction)
-	if !ok {
-		return fmt.Errorf("calling non-function")
-	}
+func (vm *VM) executeCall(numArgs int) error {
+	callee := vm.stack[vm.sp-1-numArgs]
+	switch callee := callee.(type) {
+	case *object.Builtin:
+		return vm.callBuiltin(callee, numArgs)
+	case *object.CompiledFunction:
+		return vm.callFunction(callee, numArgs)
+	default:
+		return fmt.Errorf("Calling non-function and non-builtin")
 
+	}
+}
+
+func (vm *VM) callFunction(fn *object.CompiledFunction, numArgs int) error {
 	if numArgs != fn.NumParameters {
 		return fmt.Errorf("wrong number of arguments: want=%d, got=%d",
 			fn.NumParameters, numArgs)
@@ -259,6 +277,24 @@ func (vm *VM) callFunction(numArgs int) error {
 
 	vm.pushFrame(frame)
 	vm.sp = frame.basePointer + fn.NumLocals
+	return nil
+}
+
+func (vm *VM) callBuiltin(builtin *object.Builtin, numArgs int) error {
+	// Contrary to normal function, builtins can be execute immediately.
+	args := vm.stack[vm.sp-numArgs : vm.sp]
+
+	result := builtin.Fn(args...)
+
+	// Restore the stack pointer
+	vm.sp = vm.sp - numArgs - 1
+
+	if result != nil {
+		vm.push(result)
+	} else {
+		vm.push(Null)
+	}
+
 	return nil
 }
 
